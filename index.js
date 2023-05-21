53b08c195f334e617f918c2a35b351229c6e0923
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-05-21 13:55:42 +0600

    delete lego

--- index.js
+++ index.js
@@ -97,7 +97,12 @@ const run = async () => {
             res.send(result);
         });
 
-
+        app.delete('/legos/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await legos.deleteOne(query);
+            res.send(result);
+        });
 
 
 
